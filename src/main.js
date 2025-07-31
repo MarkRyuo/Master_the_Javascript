f1afa3236d7cc6defff51d775d344958f9bc7009
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-07-31 19:36:50 +0800

    changing main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './style.css'
-import { functclickme } from './clickme';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    
-  </div>
+    <input type="email" name="email" placeholder="your@email.com" size="30" required id="email>
+    <input type="password" name="password" size="20" required id="password">
+    <button type="button" name="login" autofocus id="login">
+	    Login
+    </button>
+    </div>
 `
-
-const button = document.getElementById("clkme");
-functclickme(button);
