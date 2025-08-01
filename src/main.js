63e5690728808eb932c6e230290ec2f572f5ee86
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-01 09:44:15 +0800

    Subject: Content

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <input type="email" name="email" placeholder="your@email.com" size="30" required id="email>
-    <input type="password" name="password" size="20" required id="password">
+    <input type="email" name="email" placeholder="your@email.com" required id="email" size="30">
+    <input type="password" name="password" placeholder="your password" required id="password" size="20">
     <button type="button" name="login" autofocus id="login">
 	    Login
     </button>
