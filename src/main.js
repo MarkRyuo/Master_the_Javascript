82f1ecfc8854629a0c3dba02bd382f0d06e57259
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-01 15:15:21 +0800

    Subject: Content

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
+    <input type="text" id="characterDragonball" placeholder="Enter Dragonball character">
+    <button type="button">Fetch Data</button> <br>
 
+    <img src="" alt="Dragonball Image" style="display:none">
   </div>
 `
 
