d406a042e85c7dcc4dbb15a0b44525d29857812f
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-01 15:32:57 +0800

    Subject: Content

--- src/dragonball.js
+++ src/dragonball.js
@@ -2,7 +2,8 @@
 export async function Dragonball() {
     
     try {
-        const response = await fetch("https://dragonball-api.com/api/characters");
+        const characterDragonball = document.getElementById("characterDragonball").value.toLowerCase;
+        const response = await fetch(`https://dragonball-api.com/api/characters${characterDragonball}`);
 
         if(!response.ok){
             throw new Error("Could not Fetch data!")
@@ -10,6 +11,13 @@ export async function Dragonball() {
 
         const data = await response.json()
         console.log(data)
+
+        const dragonballChar = data.items.image
+        const dragonballImg = document.getElementById("dragonballImg");
+        
+        dragonballImg.src = dragonballChar ;
+
+        dragonballImg.style.display = "Block";
 
     } catch (error) {
         console.error(error)
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './style.css';
+import { Dragonball } from './dragonball';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
     <input type="text" id="characterDragonball" placeholder="Enter Dragonball character">
     <button type="button">Fetch Data</button> <br>
 
-    <img src="" alt="Dragonball Image" style="display:none">
+    <img src="" alt="Dragonball Image" style="display:none" id="dragonballImg">
   </div>
 `
+
+Dragonball();
 
