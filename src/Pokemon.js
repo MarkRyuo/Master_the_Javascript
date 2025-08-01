bb844d5d157eead34e95c188bcc9003d45a46d81
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-01 11:43:45 +0800

    Subject: Content

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -3,7 +3,14 @@
 export function Pokemon() {
 
     fetch("https://pokeapi.co/api/v2/pokemon/pikachu") 
-    .then(response => console.log(response))
+    .then(response => {
+
+        if(!response.ok) {
+            throw new Error("Could not fetch data");
+        }
+        return response.json()
+    })
+    .then(data => console.log(data))
     .catch(error => console.error(error));
 
 }
