d3cf2c47842eaceb8bcbc3c885310494f8ae58a8
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-01 21:07:15 +0800

    Subject: Content

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import './style.css';
-import { Dragonball } from './dragonball';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <input type="text" id="characterDragonball" placeholder="Enter Dragonball character">
-    <button type="button" id="fetchbtn">Fetch Data</button> <br>
-
-    <img src="" alt="Dragonball Image" style="display:none" id="dragonballImg">
+    
   </div>
 `
-
-document.getElementById("fetchbtn").addEventListener("click", Dragonball)
