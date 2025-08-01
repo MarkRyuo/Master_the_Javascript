5d1e6a39e9b3605696e9f7d88c69c3a2d61408ba
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-01 14:31:25 +0800

    Subject: Content

--- src/email.js
+++
@@ -1,9 +0,0 @@
-
-
-export function CheckEmail(getemail) {
-    
-    fetch("src/user.json")
-    .then(response => response.json())
-    .then(data => console.log(data.userPersonalProfile.fullname))
-    .catch(error => console.error("API HAS ERROR", error));
-}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
-import { CheckEmail } from './email';
-import { Pokemon } from './Pokemon';
-
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <input type="text" placeholder="Enter pokemon name" required id="pokemonName" size="30">
-    <button type="button" autofocus id="btnfetch">
-	    fetch data
-    </button> <br>
-
-    <img src="" alt="Pokemon Photo" id="pokemonImg">
     </div>
 `
 
 
 //CheckEmail()
 
-document.getElementById("btnfetch").addEventListener("click", Pokemon);
