1aac8eb1fe432e9f9f63c0704350015739f24ce8
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-03 17:19:08 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import { Login } from './login';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <input type="email" name="email" placeholder="your@email.com" size="30" required id="inputEmail"> <br>
-    <input type="password" name="password" placeholder="your password" size="20" required id="inputPassword"> <br>
-    <button type="button" name="buttonlogin" autofocus id="btnLogin">
-	    Login
-    </button>
 
   </div>
 `
-
-document.getElementById("btnLogin").addEventListener("click", Login)
