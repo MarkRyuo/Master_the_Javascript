3b0f6865106c8d2ea5355fbc065938ce8c31da30
MarkRyuo <malupajhonmark3035@gmail.com>
2025-08-10 15:38:10 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <form id="formLogin>
+    <form id="formLogin">
       <input type="text" name="inputUsername" placeholder="Enter Username" id="inptUsername">
-      <input type="password" name="inputPassword" placeholder="Enter Password" id="inptPassword>
-      <button type="button" id="btnLogin>
+      <input type="password" name="inputPassword" placeholder="Enter Password" id="inptPassword">
+      <button type="button" id="btnLogin">
         Login
       </button>
     </form>
