eac8965e5941605d60113515c7c561b702d5abc7
MarkRyuo <malupajhonmark3035@gmail.com>
2025-08-10 15:47:58 +0800

    Subject: Update

--- src/login.js
+++ src/login.js
@@ -1,1 +1,25 @@
+import { esbuildVersion } from "vite";
 
+
+export async function Login(params) {
+    
+    const inptUsername = document.getElementById("inptUsername");
+    const inptPassword = document.getElementById("inptPassword");
+
+    try {
+        
+        const response = await fetch("./user.json"); //* fetch the data
+        console.log(response);
+
+        if(response.ok){
+            return response.json();
+        }else {
+            throw new Error("Could not fetch data");
+        }
+
+        
+    } catch (error) {
+        console.error(error, "Something Wrong")
+    }
+
+}
